bf00776ac0be7b252d883a4532ca37e5afb9b22a
Ritesh Pampanaboyina <pritesh2078@gmail.com>
2024-06-19 13:25:27 +0530

    assessment-1

---
+++ multiplication.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class question4
+{
+    public static void Main(string[] args)
+    {
+        int x;
+        int result;
+
+        Console.WriteLine("Enter the number:");
+        x = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Multiplication table for {0}:", x);
+        Console.WriteLine("-----------------------------");
+
+        for (int i = 1; i <= 10; i++)
+        {
+            result = x * i;
+            Console.WriteLine("{0} * {1} = {2}", x, i, result);
+        }
+    }
+}
---
+++ rmvchar.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace cc1
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(RemoveCharacter("python", 1));
+            Console.WriteLine(RemoveCharacter("python", 0));
+            Console.WriteLine(RemoveCharacter("python", 4));
+            Console.ReadLine();
+
+        }
+        static string RemoveCharacter(string str, int position)
+        {
+            if (position < 0 || position >= str.Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(position));
+            }
+            return str.Remove(position, 1);
+
+        }
+    }
+}
